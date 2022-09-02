e0d339ffaeb49f17f7fb97b174267371f77c2672
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2022-09-02 09:19:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      "name":{ "firstName": "Gustavo", "lastName": "Rocha" },
+      "company":"ZTM"
+    };
+  }
+  componentDidMount() {
+    console.log("Montei!")
+  }
+  componentDidUpdate() {
+    console.log("Fui atualizado!")
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> Hi, i'm {this.state.name.firstName } {this.state.name.lastName}, I work at {this.state.company} </p>
+          <button onClick={() => {
+            this.setState(
+            () => {
+              // Works in an asynchronous way;
+              return {
+                  name: { firstName: 'Andrei', lastName: 'Neaogie' }
+                };
+              }, 
+            () => {
+              // Only is called when the state is updated;
+                console.log(this.state);
+              }
+            );
+          }}> Change Name </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
